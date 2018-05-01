ee71f49bc0c16b4f5ba922e3d90020db923385c5
Mario AYR <mario.arroyo.benito@alumnos.upm.es>
2018-05-01 18:41:40 +0200

    terminado?

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const Sequelize = require("sequelize");
+const {models} = require('../models');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req,res,next) {
+  res.render('index', {title : 'Quiz'});
+});
+router.get('/credits', function(req, res, next){
+  res.render('credits', {title: 'Mario Arroyo'});
+});
+router.get('/quizzes', function(req, res, next){
+  models.quiz.findAll()
+      .then(quizzes => {
+        res.render('quizzes', {quizzes});
+    })
+    .catch(error => next(error))
 });
 
 module.exports = router;
+
+
